4a099acbbbe17045a527d977ae65a8037a477c20
MichaelPogi <147032016+MichaelSadiwa@users.noreply.github.com>
2025-05-21 01:42:00 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -14,7 +14,7 @@ dfa_1 = {
     "states": ["q0", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11", "q12", "q13", "q14", "q15", "q16", "q17", "q18", "q19", "q20"],
     "alphabet": ["a", "b"],
     "start_state": "q0",
-    "end_states": ["q17", "q18"],
+    "end_states": ["q17", "q18", "q19", "q20"],
     "transitions": {
         # Initial prefix of (a+b)* leading to aa or bb
         ("q0", "a"): "q1",
