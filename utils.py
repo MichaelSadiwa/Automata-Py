8c5a89712c6b5b3105cf976a23ada28de1b5b7e4
MichaelPogi <147032016+MichaelSadiwa@users.noreply.github.com>
2025-05-21 16:20:15 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -23,8 +23,6 @@ dfa_1 = {
         
         # (aa+bb) detection
         ("q1", "a"): "q3",  # aa
-        ("q1", "b"): "q2",
-        ("q2", "a"): "q1",
         ("q2", "b"): "q4",  # bb
         
         # (aa+bb)* looping
